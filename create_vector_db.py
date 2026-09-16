53b4c56147e35f156d08ef4f4a638e32c1f3762c
erena <eren.akkaya@hotmail.com>
2026-09-16 03:16:05 +0300

    fixed typo, changed pdf parsing to lib

--- create_vector_db.py
+++ create_vector_db.py
@@ -1,35 +1,36 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, HTMLHeaderTextSplitter
 import pandas as pd
-from pypdf import PdfReader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader
 
 splitter = RecursiveCharacterTextSplitter(
-    chunk_size=80,
-    chunk_overlap=20,
+    chunk_size=600,
+    chunk_overlap=70,
     separators=["\n\n", "\n", ".", " ", ""]
 )
 
 embeddings = GoogleGenerativeAIEmbeddings(model = "gemini-embedding-001")
 
-def parse(document):
+def parse_split(document):
     if document.endswith(".pdf"):
-            reader = PdfReader(document)
-            parsed = ""
-            for num in range(len(reader.pages)):
-                page = reader.get_page(num)
-                if page.extract_text() is not None:
-                    parsed = parsed + page.extract_text()
+            loader = PyPDFLoader(document)
+            documents = loader.load()
+            chunks = splitter.split_documents(documents)
     else:
          with open(document, "r") as doc:
-            parsed = "".join(row for row in doc)            
+            parsed = "".join(row for row in doc)
+            documents = Document(page_content=parsed)
+            chunks = splitter.split_documents([documents])  
+
+    return chunks          
 
          
 
-def create_db(text):        
-    chunks = splitter.split_text(text)
+def create_db(document):
+    chunks = parse_split(document)
     pass
 
-def update_db(text):         
-    chunks = splitter.split_text(text)
+def update_db(document):
+    chunks = parse_split(document)
     pass
